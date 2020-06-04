d8d9814281ab1af24bcb5c8232b7eb3797a2081f
Cintia <cintia-ssouza@hotmail.com>
2020-06-04 16:34:50 -0300

    save

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const server = express()
 
-server.get("/", (req, res) => {
+server.use(express.static("public"))
 
+server.get("/index", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
 })
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+  res.sendFile(__dirname + "/views/search-results.html")
+})
+
 
 server.listen(3000)
